3e2992758490bd513ef07d7552bd9797b05641fc
nuesslerm <markus@choco.com>
2020-12-29 23:37:49 +0100

    added readme

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,18 @@
-print("hello world")
+print("Do you deserve christmas shoes? Let's find out!!")
+
+input("Press 'Enter' to continue.")
+
+answer = None
+
+validAnswers = ["y", "Y", "n", "N", ""]
+
+while answer not in validAnswers:
+    answer = input("You a pusheen? (y/N)\n")
+    pass
+
+if answer == "y" or answer == "Y":
+    print("Nooooooooo, f- off 🚧")
+    pass
+else:
+    print("I guess")
+    pass
