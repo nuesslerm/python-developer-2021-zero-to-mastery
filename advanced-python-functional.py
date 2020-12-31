42e29cf9b49f0cdde845b301c74e399d3e6123d1
nuesslerm <markus@choco.com>
2020-12-31 03:03:02 +0100

    start advanced python topic: functional programming 2

---
+++ advanced-python-functional.py
@@ -0,0 +1,12 @@
+# what is functional programming?
+# separation of data and functions
+# emphasis on simplicity where data and functions are concerned
+# functions operate on well-defined data structures rather than assigning the data structure to an object
+# general goals:
+# clear + understandable
+# easy to extend
+# easy to maintain
+# memory efficient
+# DRY
+
+# pure functions
