5d4b17f739b02214ee6e69bda670ffaff8e9ac38
nuesslerm <markus@choco.com>
2021-01-02 13:31:45 +0100

    start advanced python topic: python unit testing 3

--- modules/random_game/random_game_pure.py
+++ modules/random_game/random_game_pure.py
@@ -2,42 +2,104 @@ from sys import argv, exit
 from random import randint
 
 
-argv_len = len(argv[1:])
-
-if argv_len > 2:
-    print("Please enter a max of two numbers after the script name")
-    exit()
-
-try:
-    first_num = int(argv[1] if argv_len > 0 else 0)
-    second_num = int(argv[2] if argv_len > 1 else 10)
-except ValueError:
-    print("Please enter only integers for script args!")
-    exit()
-
-to_guess = randint(first_num, second_num)
-print(to_guess)
-
-while True:
+def assess_string_to_int(string):
     try:
-        parsed_input = float(
-            input(f"Guess an integer between {first_num} and {second_num}: ")
-        )
-        if parsed_input % 1 != 0:
-            print("Please enter an integer")
-            continue
+        int(string)
+        return True
+    except ValueError:
+        return False
 
-        if not first_num <= parsed_input <= second_num:
-            print("Your guess is out of bounds of the guessing range")
-            continue
 
-        guess = int(parsed_input)
+def assess_string_to_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
+def check_for_errors_on_script_start(args):
+    args_len = len(args[1:])
+
+    if args_len > 2:
+        print("Please enter a max of two numbers after the script name")
+        return True
+    for item in args:
+        if assess_string_to_int(item):
+            print("Please enter only integers for script args!")
+            return True
+    return False
+
+
+def check_for_errors_on_guess(float_input, first_num, second_num):
+    if float_input % 1 != 0:
+        print("Please enter an integer")
+        return True
+    if not first_num <= float_input <= second_num:
+        print("Your guess is out of bounds of the guessing range")
+        return True
+    return False
+
+
+def get_numbers(args):
+    args_len = len(args[1:])
+
+    first_num = int(args[1]) if args_len > 0 else 0
+    second_num = int(args[2]) if args_len > 1 else 10
+
+    return (first_num, second_num)
+
+
+def get_rand_int(first_num, second_num):
+    return randint(first_num, second_num)
+
+
+def parse_input_to_float(input):
+    if assess_string_to_float(input):
+        return float(input)
+    else:
+        print("Please enter an integer")
+        return None
+
+
+def guess_numbers(float_input, to_guess):
+    try:
+        guess = int(float_input)
+
         if to_guess == guess:
             print("Congrats. You got the number right!")
-            break
+            return True
         else:
             print("Sorry. The number is wrong. Try again!")
-            continue
+            return False
     except ValueError:
         print("Please enter an integer")
-        continue
+        return False
+
+
+def main():
+    if check_for_errors_on_script_start(argv):
+        exit()
+
+    first_num, second_num = get_numbers(argv)
+    to_guess = get_rand_int(first_num, second_num)
+
+    while True:
+        answer = input(f"Guess an integer between {first_num} and {second_num}: ")
+
+        float_input = parse_input_to_float(answer)
+
+        if not float_input:
+            continue
+
+        if check_for_errors_on_guess(float_input, first_num, second_num):
+            continue
+
+        if guess_numbers(float_input, to_guess):
+            break
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    main()
--- modules/random_game/random_game_pure_test.py
+++ modules/random_game/random_game_pure_test.py
@@ -2,13 +2,38 @@
 # https://docs.python.org/3/library/unittest.html
 # https://docs.python.org/3/library/unittest.html#assert-methods
 import unittest
-import random_game_pure
+from random_game_pure import guess_numbers
 
 
+# Important: By default, only functions whose name that start with test are run
 class TestRandomGame(unittest.TestCase):
+    def test_guess_numbers1(self):
+        """
+        should return False if input is not convertible to int
+        """
+        float_input = "asdf"
+        to_guess = 8
+        result = guess_numbers(float_input, to_guess)
+        self.assertEqual(result, False)
 
-    
-    
-    
+    def test_guess_numbers2(self):
+        """
+        should return False if guess is incorrect
+        """
+        float_input = 5
+        to_guess = 6
+        result = guess_numbers(float_input, to_guess)
+        self.assertEqual(result, False)
+
+    def test_guess_numbers3(self):
+        """
+        should return True if guess is correct
+        """
+        float_input = 5
+        to_guess = 5
+        result = guess_numbers(float_input, to_guess)
+        self.assertEqual(result, True)
+
+
 if __name__ == "__main__":
     unittest.main()
